e2113e4a34f1a4cf76b078346708532365364c57
Scott Smith <scott@disqus.com>
2012-01-02 22:27:00 -0800

    refactor check output handling, support multiple targets

--- check_graphite.py
+++ check_graphite.py
@@ -22,23 +22,21 @@ NAGIOS_STATUSES = {
 
 class Graphite(object):
 
-    def __init__(self, url, target, time_from, time_until):
+    def __init__(self, url, targets, time_from, time_until):
         self.url = url.rstrip('/')
-        self.target = target
+        self.targets = targets
         self.time_from = time_from
         self.time_until = time_until
+        params = [('target', t) for t in self.targets] +\
+          [('from', self.time_from)] +\
+          [('until', self.time_until)] +\
+          [('format', 'json')]
         self.full_url = self.url + '/render?' +\
-            urllib.urlencode({
-                'target': self.target,
-                'from': self.time_from,
-                'until': self.time_until,
-                'format': 'json'
-            })
+            urllib.urlencode(params)
 
-    def check_datapoints(self, datapoints, threshold, func):
-       def within_bounds(val):
-           return func(val, threshold)
-       return filter(within_bounds, datapoints)
+    def check_datapoints(self, datapoints, func, **kwargs):
+        if kwargs.get('threshold'):
+            return [x for x in datapoints if func(x, kwargs['threshold'])]
 
     def fetch_metrics(self):
         try:
@@ -51,13 +49,34 @@ class Graphite(object):
         except urllib2.URLError:
             return None
 
-def main():
+    def generate_output(self, oob, all_points, **kwargs):
+        check_output = dict(OK=[], WARNING=[], CRITICAL=[])
+        warning = kwargs['warning']
+        critical = kwargs['critical']
+        threshold = kwargs.get('threshold', None)
+        target = kwargs.get('target', 'timeseries')
+
+        if len(oob) > critical:
+            check_output['CRITICAL'].append('%s out of bounds [threshold=%s|maxcrit=%d|datapoints=%s]' %\
+                (target, str(threshold), critical, ','.join(['%s' % str(x) for x in oob])))
+        elif len(oob) > warning:
+            check_output['WARNING'].append('%s out of bounds [threshold=%s|maxwarn=%d|datapoints=%s]' %\
+                (target, str(threshold), warning, ','.join(['%s' % str(x) for x in oob])))
+        else:
+            check_output['OK'].append('%s OK [threshold=%s|maxwarn=%d|maxcrit=%d|datapoints=%s]' %\
+                (target, str(threshold), warning, critical, ','.join(['%s' % str(x) for x in all_points])))
+
+        return check_output
+
+
+def do_checks():
     parser = optparse.OptionParser()
     parser.add_option('-U', '--graphite-url', dest='graphite_url',
                       default='http://localhost/',
                       metavar='URL',
                       help='Graphite URL [%default]')
-    parser.add_option('-t', '--target', dest='target',
+    parser.add_option('-t', '--target', dest='targets',
+                      action='append',
                       help='Target to check')
     parser.add_option('--from', dest='time_from',
                       help='From timestamp/date')
@@ -93,21 +112,21 @@ def main():
 
     (options, args) = parser.parse_args()
 
-    for mandatory in ['time_from', 'target', 'threshold']:
+    for mandatory in ['time_from', 'targets', 'threshold']:
         if not options.__dict__[mandatory]:
-            print 'ERROR: missing option: --%s\n' % mandatory.replace('time_', '')
+            print 'ERROR: missing option: --%s\n' % mandatory.replace('time_', '').replace('targets', 'target')
             parser.print_help()
-            return NAGIOS_STATUSES['UNKNOWN']
+            sys.exit(NAGIOS_STATUSES['UNKNOWN'])
 
     if options.over and options.under:
         print 'ERROR: --over and --under are mutually exclusive\n'
         parser.print_help()
-        return NAGIOS_STATUSES['UNKNOWN']
+        sys.exit(NAGIOS_STATUSES['UNKNOWN'])
 
     if options.percentile:
-        target = 'nPercentile(%s, %d)' % (options.target, options.percentile)
+        targets = ['nPercentile(%s, %d)' % (options.targets[0], options.percentile)]
     else:
-        target = options.target
+        targets = options.targets
 
     try:
         if options.threshold.endswith('%'):
@@ -125,38 +144,40 @@ def main():
     except:
         print 'ERROR: threshold is not a number\n'
         parser.print_help()
-        return NAGIOS_STATUSES['UNKNOWN']
+        sys.exit(NAGIOS_STATUSES['UNKNOWN'])
 
-    graphite = Graphite(options.graphite_url, target, options.time_from, options.time_until)
+    check_output = {}
+    graphite = Graphite(options.graphite_url, targets, options.time_from, options.time_until)
     metric_data = graphite.fetch_metrics()
 
     if metric_data:
         for target in metric_data:
             datapoints = [x[0] for x in target.get('datapoints', []) if x]
-            if datapoints:
-                points_oob = graphite.check_datapoints(datapoints, check_threshold, check_func)
+            oob = graphite.check_datapoints(datapoints, check_func, threshold=check_threshold)
+            check_output[target['target']] = graphite.generate_output(oob, datapoints,
+                                                                      target=target['target'],
+                                                                      threshold=check_threshold,
+                                                                      critical=options.critical,
+                                                                      warning=options.warning)
 
-                if len(points_oob) > options.critical:
-                    print 'CRITICAL: timeseries out of bounds [threshold=%0.3f|maxcrit=%d|datapoints=%s]' %\
-                        (check_threshold, options.critical, ','.join(['%s' % str(x) for x in points_oob]))
-                    status = 'CRITICAL'
-                elif len(points_oob) > options.warning:
-                    print 'WARNING: timeseries out of bounds [threshold=%0.3f|maxwarn=%d|datapoints=%s]' %\
-                        (check_threshold, options.warning, ','.join(['%s' % str(x) for x in points_oob]))
-                    status = 'WARNING'
-                else:
-                    print 'OK: timeseries OK [threshold=%0.3f|maxwarn=%d|maxcrit=%d|datapoints=%s]' %\
-                        (check_threshold, options.warning, options.critical, ','.join(['%s' % str(x) for x in datapoints]))
-                    status = 'OK'
-            else:
-                print 'CRITICAL: No datapoints for target %s!' % target.get('target')
-                status = 'CRITICAL'
-
+        return check_output
     else:
         print 'CRITICAL: No output from Graphite!'
-        status = 'CRITICAL'
-
-    return NAGIOS_STATUSES[status]
+        sys.exit(NAGIOS_STATUSES['CRITICAL'])
 
 if __name__ == '__main__':
-  sys.exit(main())
+    output = do_checks()
+
+    for target, messages in output.iteritems():
+        if messages['CRITICAL']:
+            exit_code = NAGIOS_STATUSES['CRITICAL']
+        elif messages['WARNING']:
+            exit_code = NAGIOS_STATUSES['WARNING']
+        else:
+            exit_code = NAGIOS_STATUSES['OK']
+
+        for status_code in ['CRITICAL', 'WARNING', 'OK']:
+            if messages[status_code]:
+                print '\n'.join(['%s: %s' % (status_code, status) for status in messages[status_code]])
+
+    sys.exit(exit_code)
